e3edee4a2bef0557edf84924af098266b27bec02
danyloy77 <danylo.ya@corefy.com>
2023-09-21 00:42:46 +0300

    added variable.js

---
+++ js-variables/variables.js
@@ -0,0 +1,14 @@
+let adminName = "Liubov";
+console.log(typeof adminName);
+
+let userName = "Danya";
+console.log(typeof userName);
+
+let status = "awesome";
+let num1 = 12;
+let num2 = 7;
+console.log(`Javascript is: ${status}, ${num1}, ${num2}`);
+
+let password = "qwerty";
+password = "intiti";
+console.log(password)
