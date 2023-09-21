fe7763a2477f1ace79d2d52fece646da034df19c
danyloy77 <145136016+danyloy77@users.noreply.github.com>
2023-09-21 18:46:55 +0300

    added variable.js (#2)
    * added variable.js
    
    * fixed bug

---
+++ js-variables/variables.js
@@ -0,0 +1,12 @@
+let adminName = "Liubov";
+let userName = "Danya";
+console.log(typeof userName, typeof adminName);
+
+let status = "awesome";
+let num1 = 12;
+let num2 = 7.3;
+console.log(`Javascript is: ${status}, ${num1}, ${num2}`);
+
+let password = "qwerty";
+password = "intiti";
+console.log("password");
